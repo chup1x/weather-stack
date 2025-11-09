ada60d302d0ffd9fec3c256cf6111c513ca5b826
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 19:30:22 +0300

    Update 000001_users.up.sql
    users update

--- backend/migrations/000001_users.up.sql
+++ backend/migrations/000001_users.up.sql
@@ -2,13 +2,21 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users(
 	id          UUID         PRIMARY KEY,
-	login       VARCHAR(255) NOT NULL UNIQUE,
 	name        VARCHAR(100) NOT NULL,
 	sex         VARCHAR(5)   NOT NULL CHECK(sex IN('male', 'female')),
 	age         INTEGER      NOT NULL CHECK(age > 0),
-	city        VARCHAR(50)  NOT NULL DEFAULT '',
+	city_n      VARCHAR(50)  NOT NULL DEFAULT '',
+	city_w      VARCHAR(50)  NOT NULL DEFAULT '',
+	drop_time   VARCHAR(10)  NOT NULL DEFAULT '10/00',
+	t_comfort   INTEGER		 NOT NULL DEFAULT 24,
+	t_tol	    INTEGER		 NOT NULL DEFAULT 18,
+	t_puh	    INTEGER		 NOT NULL DEFAULT 6,
+	temp1		INTEGER		 NOT NULL DEFAULT 0,
+	temp2   	VARCHAR(50)  NOT NULL DEFAULT '',
+
+	password    VARCHAR(100) NOT NULL DEFAULT 'bot_user',
 	
-	telegram_id BIGINT       NOT NULL DEFAULT 0,
+	telegram_id INTEGER       NOT NULL DEFAULT 0,
 	
 	created_at  TIMESTAMP    NOT NULL DEFAULT now()
 );
