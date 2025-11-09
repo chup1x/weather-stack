e514444e7b23da78865e326863041c74f51fedfc
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 19:40:29 +0300

    Update user_entity.go

--- backend/internal/domain/user_entity.go
+++ backend/internal/domain/user_entity.go
@@ -15,11 +15,17 @@ type UserID struct {
 
 type UserEntity struct {
 	ID         UserID `json:"id" gorm:"id"`
-	Login      string `json:"login" gorm:"login"`
 	Name       string `json:"name" gorm:"name"`
 	Sex        string `json:"sex" gorm:"sex"`
 	Age        int    `json:"age" gorm:"age"`
-	City       string `json:"city" gorm:"city"`
+	City_n     string `json:"city_n" gorm:"city_n"`
+	City_w     string `json:"city_w" gorm:"city_w"`
+	Drop_time  string `json:"drop" gorm:"drop"`
+	T_comf	   int	  `json:"t_com" gorm:"t_com"`
+	T_tol	   int	  `json:"t_tol" gorm:"t_tol"`
+	T_puh	   int	  `json:"t_puh" gorm:"t_puh"`
+	Passw      string `json:"sex" gorm:"sex"`
+	
 	TelegramID int64  `json:"telegram_id" gorm:"telegram_id"`
 
 	CreatedAt time.Time `json:"created_at" gorm:"created_at"`
