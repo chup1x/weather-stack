ccafe785d05360d843e96bf7189aca0eca1a032c
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 20:30:44 +0300

    Update model.go
    Weather update

--- backend/internal/transport/v1/rest/weather/model.go
+++ backend/internal/transport/v1/rest/weather/model.go
@@ -1,5 +1,21 @@
 package weathercntrl
 
+type GetWeatherHistoryRequest struct {
+	ID int `param:"city_w" validate:"required"`
+}
+
+type GetWeatherHistoryResponse struct {
+	*domain.WeatherEntity
+}
+
+type GetWeatherClothesRequest struct {
+	ID string `json:"combination" validate:"required"`
+}
+
+//type GetWeatherClothesResponse struct {
+	//*domain.WeatherClothesEntity
+//}
+
 type CreateWeatherRecord struct {
 	TelegramID  int64   `json:"telegram_id"`
 	Temperature float64 `json:"temperature"`
