11ee9f6f5350b7693ab4461c4724468890dbaa81
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 20:44:00 +0300

    Update controller.go
    Weather update

--- backend/internal/transport/v1/rest/weather/controller.go
+++ backend/internal/transport/v1/rest/weather/controller.go
@@ -39,5 +39,48 @@ func (cn *weatherController) CreateWeatherRecordHandler(c *fiber.Ctx) error {
 }
 
 func (cn *weatherController) GetWeatherHistoryHandler(c *fiber.Ctx) error {
-	return c.SendStatus(fiber.StatusOK)
+	
+	var req GetWeatherHistoryRequest
+	if err := c.ParamsParser(&req); err != nil {
+		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+	if err := cn.validator.Struct(req); err != nil {
+		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+
+	city, err := cn.s.GetWeather(c.UserContext(), req.City_w)
+	if err != nil {
+		if errors.Is(err, domain.ErrUserNotFound) {
+			return c.SendStatus(fiber.StatusNotFound)
+		}
+		log.Error(err.Error())
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	res := GetWeatherHistoryResponse{city}
+
+	return c.JSON(res)
+}
+
+func (cn *userController) GetWeatherClothesHandler(c *fiber.Ctx) error {
+	var req GetWeatherColthesRequest
+	if err := c.BodyParser(&req); err != nil {
+		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+	if err := cn.validator.Struct(req); err != nil {
+		return c.SendStatus(fiber.StatusUnprocessableEntity)
+	}
+
+	user, err := cn.s.GetWeatherClothes(c.UserContext(), req.user)
+	if err != nil {
+		if errors.Is(err, domain.ErrUserNotFound) {
+			return c.SendStatus(fiber.StatusNotFound)
+		}
+		log.Error(err.Error())
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	res := GetWeatherClothesResponse{user}
+
+	return c.JSON(res)
 }
