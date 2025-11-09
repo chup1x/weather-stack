6d62d444564506fe01b46b3c69edb3c6a5ee2f18
AArtemBashta <60484170+StranaGedon-ru@users.noreply.github.com>
2025-11-09 20:50:22 +0300

    Update model.go

--- backend/internal/transport/v1/rest/weather/model.go
+++ backend/internal/transport/v1/rest/weather/model.go
@@ -12,9 +12,17 @@ type GetWeatherClothesRequest struct {
 	ID string `json:"combination" validate:"required"`
 }
 
-//type GetWeatherClothesResponse struct {
-	//*domain.WeatherClothesEntity
-//}
+type GetWeatherClothesResponse struct {
+	*domain.WeatherClothesEntity
+}
+
+type GetNewsRequest struct {
+	ID string `json:"city_n" validate:"required"`
+}
+
+type GetNewsResponse struct {
+	*domain.NewsEntity
+}
 
 type CreateWeatherRecord struct {
 	TelegramID  int64   `json:"telegram_id"`
